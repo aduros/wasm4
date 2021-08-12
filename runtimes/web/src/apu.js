30bf407498ef880c9e903982c3984e9ed585eb3a
Bruno Garcia <b@aduros.com>
2021-08-12 16:40:51 -0300

    Rework APU.
    - Create a new node every time to hopefully eliminate jank.
    - Fix bugs on Firefox.

--- runtimes/web/src/apu.js
+++ runtimes/web/src/apu.js
@@ -20,57 +20,30 @@ const NOISE_LENGTH = 0x8000;
 //     440.0 / 44100,
 // ].reverse();
 
-class Channel {
-    constructor (ctx, node) {
-        this.node = node;
-        this.gainLevel = 1;
+export class APU {
+    constructor () {
+        const ctx = new AudioContext();
+        this.ctx = ctx;
 
-        const gain = ctx.createGain();
-        this.gain = gain;
+        this.nodes = new Array(4);
 
-        gain.gain.value = 0;
-        gain.connect(ctx.destination);
-
-        node.connect(gain);
-        node.start(0);
-    }
-
-    setEnvelope (currentTime, sustainLevel, attack, decay, sustain, release) {
-        const peakLevel = this.gainLevel * 2*sustainLevel;
-        sustainLevel *= this.gainLevel;
-
-        const gainValue = this.gain.gain;
-        gainValue.cancelScheduledValues(0);
-        gainValue.value = 0;
-        gainValue.linearRampToValueAtTime(peakLevel, currentTime+attack);
-        gainValue.linearRampToValueAtTime(sustainLevel, currentTime+attack+decay);
-        gainValue.linearRampToValueAtTime(sustainLevel, currentTime+attack+decay+sustain);
-        gainValue.linearRampToValueAtTime(0, currentTime+attack+decay+sustain+release);
-    }
-}
-
-class OscillatorChannel extends Channel {
-    constructor (ctx) {
-        super(ctx, ctx.createOscillator());
-    }
-
-    setFrequency (currentTime, freq1, freq2, duration) {
-        const frequencyValue = this.node.frequency;
-        frequencyValue.cancelScheduledValues(0);
-        frequencyValue.value = freq1;
-        if (freq2) {
-            frequencyValue.linearRampToValueAtTime(freq2, currentTime + duration);
+        function createDutyCycle (pulseWidth) {
+            const real = new Float32Array(DUTY_CYCLE_LENGTH);
+            for (let ii = 1; ii < DUTY_CYCLE_LENGTH; ii++) {
+                real[ii] = 4 / (ii * Math.PI) * Math.sin(Math.PI * ii * pulseWidth);
+            }
+            return ctx.createPeriodicWave(real, new Float32Array(DUTY_CYCLE_LENGTH));
         }
-    }
-}
+        const duty25 = createDutyCycle(0.25);
+        this.pulseDutyCycles = [
+            createDutyCycle(0.125),
+            duty25,
+            createDutyCycle(0.5),
+            duty25, // 75 is the same as 25
+        ];
 
-class NoiseChannel extends Channel {
-    constructor (ctx) {
-        const node = ctx.createBufferSource();
-        super(ctx, node);
-
-        const buffer = ctx.createBuffer(1, NOISE_LENGTH, ctx.sampleRate);
-        const data = buffer.getChannelData(0);
+        const noiseBuffer = ctx.createBuffer(1, NOISE_LENGTH, ctx.sampleRate);
+        const noiseData = noiseBuffer.getChannelData(0);
         let seed = 0x0001;
         for (let ii = 0; ii < NOISE_LENGTH; ++ii) {
             const bit0 = seed & 1;
@@ -78,63 +51,12 @@ class NoiseChannel extends Channel {
             const bit1 = seed & 1;
             const feedback = (bit0 ^ bit1);
             seed |= feedback << 14;
-            data[ii] = feedback;
+            noiseData[ii] = feedback;
         }
-
-        node.buffer = buffer;
-        node.loop = true;
-    }
-
-    setFrequency (currentTime, freq1, freq2, duration) {
-        const playbackRate = this.node.playbackRate;
-        playbackRate.cancelScheduledValues(0);
-        playbackRate.value = freq1*freq1/1000000;
-        // playbackRate.value = NOISE_TABLE[16*(freq1/1024) | 0];
-        if (freq2) {
-            playbackRate.linearRampToValueAtTime(freq2*freq2/1000000, currentTime + duration);
-        }
-    }
-}
-
-function createDutyCycle (ctx, pulseWidth) {
-    const real = new Float32Array(DUTY_CYCLE_LENGTH);
-    for (let ii = 1; ii < DUTY_CYCLE_LENGTH; ii++) {
-        real[ii] = 4 / (ii * Math.PI) * Math.sin(Math.PI * ii * pulseWidth);
-    }
-    return ctx.createPeriodicWave(real, new Float32Array(DUTY_CYCLE_LENGTH));
-}
-
-export class APU {
-    constructor () {
-        const ctx = new AudioContext();
-        this.ctx = ctx;
-
-        const pulse1 = new OscillatorChannel(ctx);
-        pulse1.gainLevel = 0.5;
-
-        const pulse2 = new OscillatorChannel(ctx);
-        pulse2.gainLevel = 0.5;
-
-        const triangle = new OscillatorChannel(ctx);
-        triangle.node.type = "triangle";
-
-        const noise = new NoiseChannel(ctx);
-
-        this.channels = [pulse1, pulse2, triangle, noise];
-
-        const duty25 = createDutyCycle(ctx, 0.25);
-        this.pulseDutyCycles = [
-            createDutyCycle(ctx, 0.125),
-            duty25,
-            createDutyCycle(ctx, 0.5),
-            duty25, // 75 is the same as 25
-        ];
+        this.noiseBuffer = noiseBuffer;
     }
 
     tone (frequency, duration, volume, flags) {
-        const channelIdx = flags & 0x3;
-        const mode = (flags >> 2) & 0x3;
-
         const freq1 = frequency & 0xffff;
         const freq2 = (frequency >> 16) & 0xffff;
 
@@ -143,14 +65,70 @@ export class APU {
         const decay = ((duration >> 16) & 0xff) / 60;
         const attack = ((duration >> 24) & 0xff) / 60;
 
-        const currentTime = this.ctx.currentTime;
+        const channel = flags & 0x3;
+        const mode = (flags >> 2) & 0x3;
 
-        const channel = this.channels[channelIdx];
-        if (channelIdx == 0 || channelIdx == 1) {
-            const wave = this.pulseDutyCycles[mode];
-            channel.node.setPeriodicWave(wave);
+        const ctx = this.ctx;
+        const attackTime = ctx.currentTime + attack;
+        const decayTime = attackTime + decay;
+        const sustainTime = decayTime + sustain;
+        const releaseTime = sustainTime + release;
+
+        let peakLevel = 1;
+        let sustainLevel = volume/100;
+        if (channel == 0 || channel == 1) {
+            // Pulse waves are naturally LOUD, so half their volume
+            peakLevel *= 0.5;
+            sustainLevel *= 0.5;
         }
-        channel.setFrequency(currentTime, freq1, freq2, attack+decay+sustain+release);
-        channel.setEnvelope(currentTime, volume/100, attack, decay, sustain, release);
+
+        // Create gain node with ADSR ramp
+        const gain = ctx.createGain();
+        const gainValue = gain.gain;
+        gainValue.setValueAtTime(0, 0);
+        gainValue.linearRampToValueAtTime(peakLevel, attackTime);
+        gainValue.linearRampToValueAtTime(sustainLevel, decayTime);
+        gainValue.linearRampToValueAtTime(sustainLevel, sustainTime);
+        gainValue.linearRampToValueAtTime(0, releaseTime);
+
+        let node;
+        if (channel == 3) {
+            node = ctx.createBufferSource();
+            node.buffer = this.noiseBuffer;
+            node.loop = true;
+
+            const pbrValue = node.playbackRate;
+            pbrValue.setValueAtTime(freq1*freq1/1000000, 0);
+            // pbrValue.value = NOISE_TABLE[16*(freq1/1024) | 0];
+            if (freq2) {
+                pbrValue.linearRampToValueAtTime(freq2*freq2/1000000, releaseTime);
+            }
+
+        } else {
+            node = ctx.createOscillator();
+            if (channel == 2) {
+                node.type = "triangle";
+            } else {
+                const wave = this.pulseDutyCycles[mode];
+                node.setPeriodicWave(wave);
+            }
+
+            const freqValue = node.frequency;
+            freqValue.setValueAtTime(freq1, 0);
+            if (freq2) {
+                freqValue.linearRampToValueAtTime(freq2, releaseTime);
+            }
+        }
+
+        const existingNode = this.nodes[channel];
+        if (existingNode != null) {
+            existingNode.stop(0);
+        }
+        this.nodes[channel] = node;
+
+        node.start(0);
+        node.stop(releaseTime);
+        node.connect(gain);
+        gain.connect(ctx.destination);
     }
 }

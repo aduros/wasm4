48201179ea59e357b5dbd2f7cb2fee1199227f9c
Konstantin Timoshenko <rondo.devil@gmail.com>
2021-09-20 21:22:17 +0300

    fix: Use a maximal actual gamepad
    Reset gamepad state only when a used gamepad is dissconected.
    Use latest available gamepad.
    Avoid resseting a key state by gamepad when a keyboard is used

--- runtimes/web/src/index.js
+++ runtimes/web/src/index.js
@@ -197,8 +197,17 @@ async function loadCartWasm () {
         122: requestFullscreen, // F11
     };
 
+    let isKeyboard = true;
+
     const onKeyboardEvent = event => {
         const down = (event.type == "keydown");
+
+        if (!isKeyboard) {
+            // reset joy pad state
+            runtime.setGamepad(0,0);
+        }
+
+        isKeyboard = true;
 
         // Poke WebAudio
         runtime.unlockAudio();
@@ -245,41 +254,81 @@ async function loadCartWasm () {
     window.addEventListener("keydown", onKeyboardEvent);
     window.addEventListener("keyup", onKeyboardEvent);
 
-    let gamepads = 0;
+    let usedGamepad = -1;
+
     function processGamepad() {
-        if (gamepads === 0) {
+        if (usedGamepad === -1) {
             return;
         }
 
-        const gamepad = navigator.getGamepads()[0];
+        const gamepad = navigator.getGamepads()[usedGamepad];
         if (!gamepad) {
-
             return;
         }
 
         const buttons = gamepad.buttons;
         const axes = gamepad.axes;
+        
+        // not all gamepads map DPAD to 12,13, 14, 15 buttos
+        const dpad = buttons.length > 12;
 
         // https://www.w3.org/TR/gamepad/#remapping
         // DPAD + AXIS
-        runtime.maskGamepad(0, constants.BUTTON_UP, buttons[12].pressed || axes[1] < -0.5);
-        runtime.maskGamepad(0, constants.BUTTON_DOWN, buttons[13].pressed || axes[1] > 0.5);
-        runtime.maskGamepad(0, constants.BUTTON_LEFT, buttons[14].pressed || axes[0] < -0.5);
-        runtime.maskGamepad(0, constants.BUTTON_RIGHT, buttons[15].pressed || axes[0] > 0.5);
+        const up = +(dpad && buttons[12].pressed || axes[1] < -0.5);
+        const down = +(dpad && buttons[13].pressed || axes[1] > 0.5);
+        const left = +(dpad && buttons[14].pressed || axes[0] < -0.5);
+        const right = +(dpad && buttons[15].pressed || axes[0] > 0.5);
 
         // X, O + Triggers
         // NOTE: for XBox360 a triggers is 6 and 7
-        runtime.maskGamepad(0, constants.BUTTON_X, buttons[0].pressed || buttons[6].pressed);
-        runtime.maskGamepad(0, constants.BUTTON_Z, buttons[1].pressed || buttons[7].pressed);
+        const x = +(buttons[0].pressed || buttons[6].pressed);
+        const z = +(buttons[1].pressed || buttons[7].pressed);
+
+
+        let buttonMask = 0;
+        buttonMask |= constants.BUTTON_UP * up;
+        buttonMask |= constants.BUTTON_DOWN * down;
+        buttonMask |= constants.BUTTON_LEFT * left;
+        buttonMask |= constants.BUTTON_RIGHT * right;
+
+        buttonMask |= constants.BUTTON_X * x;
+        buttonMask |= constants.BUTTON_Z * z;
+
+        // supress changing if keyboard used
+        // we should not reset state but should read it
+        if (buttonMask !== 0 || !isKeyboard) {
+            isKeyboard = false;
+
+            runtime.setGamepad(0, buttonMask);
+        }
     }
 
     window.addEventListener("gamepadconnected", (e) => {
-        gamepads ++;
+        // find a first active gamepad
+        for(const g of navigator.getGamepads()) {
+            if (g) {
+                usedGamepad = g.index;
+                break;
+            }
+        }
     });
 
     window.addEventListener('gamepaddisconnected', (e) => {
-        gamepads --;
-        runtime.maskGamepad(0,0, false);
+        // if gamepad is same - nothing doing
+        if (e.gamepad.index !== usedGamepad) {
+            return;
+        }
+
+        // reset 
+        usedGamepad = -1;
+        runtime.setGamepad(0, 0);
+
+        for(const g of navigator.getGamepads()) {
+            if (g) {
+                usedGamepad = g.index;
+                break;
+            }
+        }
     });
 
     const dpad = document.getElementById("gamepad-dpad");

2106d76483757365295fd2a0258843f8374b9542
Konstantin Timoshenko <rondo.devil@gmail.com>
2021-09-20 20:00:17 +0300

    fix: Remove gamepad log

--- runtimes/web/src/index.js
+++ runtimes/web/src/index.js
@@ -274,15 +274,6 @@ async function loadCartWasm () {
     }
 
     window.addEventListener("gamepadconnected", (e) => {
-        const gp = navigator.getGamepads()[e.gamepad.index];
-        console.log(
-          "Gamepad connected at index %d: %s. %d buttons, %d axes.",
-          gp.index,
-          gp.id,
-          gp.buttons.length,
-          gp.axes.length
-        );
-
         gamepads ++;
     });
 

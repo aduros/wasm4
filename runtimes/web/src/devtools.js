2f223612831332a8b057f273759a360987d27937
FaberVitale <faber.vitale@gmail.com>
2021-11-28 21:29:37 +0100

    refactor(devtools): set update interval to 200ms

--- runtimes/web/src/devtools.js
+++ runtimes/web/src/devtools.js
@@ -1,6 +1,9 @@
 import { throttle } from 'lodash-es';
-import { createUpdateCompletedEvent, wasm4DevtoolsTagName, closeDevtoolsEventType } from '@wasm4/web-devtools';
-
+import {
+  createUpdateCompletedEvent,
+  wasm4DevtoolsTagName,
+  closeDevtoolsEventType,
+} from '@wasm4/web-devtools';
 
 export class DevtoolsManager {
   _enabled = false;
@@ -10,44 +13,43 @@ export class DevtoolsManager {
   }
 
   /**
-  * @type {(dataView: DataView, deltaFrame: number) => void}
-  */
+   * @type {(dataView: DataView, deltaFrame: number) => void}
+   */
   updateCompleted = (dataView, deltaFrame) => {
-    if(this._enabled) {
+    if (this._enabled) {
       const fps = Math.floor(1_000 / deltaFrame);
 
-      this._notifyUpdateCompleted(dataView, fps)
+      this._notifyUpdateCompleted(dataView, fps);
     }
-  }
+  };
 
   removeDevTools = () => {
-    if(this._enabled) {
+    if (this._enabled) {
       this.toggleDevtools();
     }
-  }
+  };
 
   /**
-  * @type {() => void}
-  */
+   * @type {() => void}
+   */
   toggleDevtools = () => {
     this._enabled = !this._enabled;
 
     const devtoolsRoot = document.getElementById('devtools');
-  
-    if(this._enabled) {
+
+    if (this._enabled) {
       devtoolsRoot.appendChild(document.createElement(wasm4DevtoolsTagName));
     } else {
       while (devtoolsRoot.firstChild) {
         devtoolsRoot.removeChild(devtoolsRoot.firstChild);
       }
     }
-
-  }
+  };
   /**
    * @private
-  * @type {(dataView: DataView) => void}
-  */
+   * @type {(dataView: DataView) => void}
+   */
   _notifyUpdateCompleted = throttle((dataView, fps) => {
     window.dispatchEvent(createUpdateCompletedEvent(dataView, fps));
-  }, 250);
+  }, 200);
 }

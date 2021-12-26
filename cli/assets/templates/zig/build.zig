994dbb5e6c09599c8c9c425b412f0f8b20b996d1
Jim Newsome <jim@sporksmith.net>
2021-12-25 19:45:02 -0600

    zig template: export only the 'start' and 'update' functions
    Workaround for https://github.com/ziglang/zig/issues/2910.
    
    See in particular:
    https://github.com/ziglang/zig/issues/2910#issuecomment-1003455815
    https://github.com/ziglang/zig/issues/2910#issuecomment-1003456922

--- cli/assets/templates/zig/build.zig
+++ cli/assets/templates/zig/build.zig
@@ -11,5 +11,9 @@ pub fn build(b: *std.build.Builder) void {
     lib.max_memory = 65536;
     lib.global_base = 6560;
     lib.stack_size = 8192;
+    // Workaround https://github.com/ziglang/zig/issues/2910, preventing
+    // functions from compiler_rt getting incorrectly marked as exported, which
+    // prevents them from being removed even if unused.
+    lib.export_symbol_names = &[_][]const u8{ "start", "update" };
     lib.install();
 }

eb0ca0c4d1ad57d4cb6f5105f0678c00428c045f
FaberVitale <faber.vitale@gmail.com>
2022-01-02 12:14:28 +0100

    perf: call wasm-opt binary directly
    Other changes:
    
    - display cart size
    - display size reduction

--- cli/lib/opt.js
+++ cli/lib/opt.js
@@ -1,12 +1,36 @@
 'use strict';
-const { spawnSync, execSync } = require('child_process');
+const { spawnSync } = require('child_process');
 const path = require('path');
 const fs = require('fs');
+const binaryenPkg = require.resolve('binaryen/package.json');
+const wasmOptBinPath = path.join(path.dirname(binaryenPkg), 'bin/wasm-opt');
+
 const { EOL } = require('os');
 
 const logError = (...args) => {
   console.error(`w4 opt error: ${args.join(EOL)}`);
-}
+};
+
+const wasmOptFlags = [
+  '-Oz',
+  '--strip-dwarf',
+  '--strip-producers',
+  '--zero-filled-memory',
+];
+
+/**
+ * @type {(bytes: number) => string}
+ */
+const formatKiB = (bytes) => {
+  return (bytes / 1024).toFixed(3) + 'KiB';
+};
+
+/**
+ * @type {(num: number, den: number) => string}
+ */
+const formatPercent = (num, den) => {
+  return `${((num / den) * 100).toFixed(2)}%`;
+};
 
 /**
  * @type{(cart: string, options: { output: string, silent: boolean }) => void}
@@ -17,55 +41,84 @@ function optimizeCart(cart, { output, silent }) {
     : path.resolve(process.cwd(), cart);
 
   const absoluteOutputCartPath = path.isAbsolute(output)
-  ? output
-  : path.resolve(process.cwd(), output);
+    ? output
+    : path.resolve(process.cwd(), output);
 
-  if(!fs.existsSync(absoluteInputCartPath)) {
-     logError(`cart "${cart}" not found.`);
+  /**
+   * @type {import('fs').Stats | null}
+   */
+  let inCartStats = null;
+  /**
+   * @type {import('fs').Stats | null}
+   */
+  let outCartStats = null;
+
+  try {
+    inCartStats = fs.statSync(absoluteInputCartPath);
+
+    if (inCartStats.isDirectory()) {
+      logError(`cart "${absoluteInputCartPath}" is a directory.`);
       process.exitCode = 1;
       return;
+    }
+  } catch (err) {
+    if (err?.code === 'ENOENT') {
+      logError(`cart "${absoluteInputCartPath}" not found.`);
+    } else {
+      logError(err);
+    }
+
+    process.exitCode = 1;
+    return;
   }
 
-   // @see https://nodejs.org/api/child_process.html#child_processspawnsynccommand-args-options
-   const wasmOptProcess = spawnSync(
-    'npx',
-    [
-      '-q',
-      '--package',
-      'binaryen',
-      '--',
-      'wasm-opt',
-      absoluteInputCartPath,
-      '-o',
-      absoluteOutputCartPath,
-      '-Oz',
-      '--strip-dwarf',
-      '--strip-producers',
-      '--zero-filled-memory',
-    ]
-  );
+  // @see https://nodejs.org/api/child_process.html#child_processspawnsynccommand-args-options
+  const wasmOptProcess = spawnSync(wasmOptBinPath, [
+    absoluteInputCartPath,
+    '-o',
+    absoluteOutputCartPath,
+    ...wasmOptFlags,
+  ]);
 
   // status is the `exitCode`
-  if(wasmOptProcess.status) {
+  if (wasmOptProcess.status) {
     process.exitCode = wasmOptProcess.status;
 
-    logError(`an error has occurred while running wasm-opt: exit code ${wasmOptProcess.status}`);
+    logError(
+      `an error has occurred while running wasm-opt. Exit code ${wasmOptProcess.status}`
+    );
 
     process.stderr.write(wasmOptProcess.stderr);
     return;
-  } 
-  
+  }
+
   // wasmOptProcess.error Error | null: `The error object if the child process failed or timed out.`
-  if(wasmOptProcess.error) {
+  if (wasmOptProcess.error) {
     process.exitCode = 1;
-    logError(`an error has occurred while running wasm-opt: ${wasmOptProcess.error}`);
+    logError(
+      `an error has occurred while running wasm-opt. Error ${wasmOptProcess.error}`
+    );
     process.stderr.write(wasmOptProcess.stderr);
 
     return;
   }
 
-  if(!silent) {
-      console.log(`w4 opt${EOL}- input:  ${absoluteInputCartPath}${EOL}- output: ${absoluteOutputCartPath}`);
+  if (!silent) {
+    outCartStats = fs.statSync(absoluteOutputCartPath);
+
+    process.stdout.write(
+      [
+        'w4 opt',
+        `- input:  ${formatKiB(inCartStats.size)} ${absoluteInputCartPath}`,
+        `- output: ${formatKiB(outCartStats.size)} ${absoluteOutputCartPath}`,
+        `size reduction: ${formatPercent(
+          inCartStats.size - outCartStats.size,
+          inCartStats.size
+        )}`,
+      ]
+        .join(EOL)
+        .concat(EOL)
+    );
   }
 }
 

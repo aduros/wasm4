dded713115e72ac8f5d72f866fce84e9d7ddf4db
Bruno Garcia <b@aduros.com>
2022-01-16 16:04:34 -0300

    Port the sample-based APU to web (#321)
    There's a bug where the triangle channel has some popping when starting.
    This is especially noticeable on low frequencies.
    
    Note that on web the sample buffer uses floats and not int16s.

--- runtimes/web/src/apu.js
+++ runtimes/web/src/apu.js
@@ -1,150 +1,188 @@
-const DUTY_CYCLE_LENGTH = 0x800;
-const NOISE_LENGTH = 0x8000;
+const SAMPLE_RATE = 44100;
+const MAX_VOLUME = 0.25;
+const BUFFER_SIZE = 512; // Might need to bump this to 1024
 
-// const NOISE_TABLE = [
-//     447443.2 / 44100,
-//     223721.6 / 44100,
-//     111860.8 / 44100,
-//     55930.4 / 44100,
-//     27965.2 / 44100,
-//     18643.5 / 44100,
-//     13982.6 / 44100,
-//     11186.1 / 44100,
-//     8860.3 / 44100,
-//     7046.3 / 44100,
-//     4709.9 / 44100,
-//     3523.2 / 44100,
-//     2348.8 / 44100,
-//     1761.6 / 44100,
-//     879.9 / 44100,
-//     440.0 / 44100,
-// ].reverse();
+class Channel {
+    /** Starting frequency. */
+    freq1 = 0;
+
+    /** Ending frequency, or zero for no frequency transition. */
+    freq2 = 0;
+
+    /** Time the tone was started. */
+    startTime = 0;
+
+    /** Time at the end of the attack period. */
+    attackTime = 0;
+
+    /** Time at the end of the decay period. */
+    decayTime = 0;
+
+    /** Time at the end of the sustain period. */
+    sustainTime = 0;
+
+    /** Time the tone should end. */
+    releaseTime = 0;
+
+    /** Sustain volume level. */
+    volume = 0;
+
+    /** Used for time tracking. */
+    phase = 0;
+
+    /** Duty cycle for pulse channels. */
+    pulseDutyCycle = 0;
+
+    /** Noise generation state. */
+    noiseSeed = 0x0001;
+
+    /** The last generated random number, either -1 or 1. */
+    noiseLastRandom = 0;
+}
+
+function lerp (value1, value2, t) {
+    return value1 + t * (value2 - value1);
+}
 
 export class APU {
     constructor () {
-        const ctx = new (window.AudioContext || window.webkitAudioContext)();
+        this.time = 0;
+        this.channels = new Array(4);
+        for (let ii = 0; ii < 4; ++ii) {
+            this.channels[ii] = new Channel();
+        }
+
+        const ctx = new (window.AudioContext || window.webkitAudioContext)({
+            sampleRate: SAMPLE_RATE,
+        });
         this.ctx = ctx;
 
-        this.nodes = new Array(4);
-        this.gains = new Array(4);
+        const scriptNode = ctx.createScriptProcessor(BUFFER_SIZE, 0, 1);
+        scriptNode.addEventListener("audioprocess", event => {
+            const output = event.outputBuffer.getChannelData(0);
 
-        function createDutyCycle (pulseWidth) {
-            const real = new Float32Array(DUTY_CYCLE_LENGTH);
-            for (let ii = 1; ii < DUTY_CYCLE_LENGTH; ii++) {
-                real[ii] = 4 / (ii * Math.PI) * Math.sin(Math.PI * ii * pulseWidth);
+            for (let ii = 0; ii < BUFFER_SIZE; ++ii, ++this.time) {
+                let sum = 0;
+
+                for (let channelIdx = 0; channelIdx < 4; ++channelIdx) {
+                    const channel = this.channels[channelIdx];
+                    if (this.time < channel.releaseTime) {
+                        const freq = this.getCurrentFrequency(channel);
+                        const volume = this.getCurrentVolume(channel);
+                        let sample;
+
+                        if (channelIdx == 3) {
+                            // Noise channel
+                            channel.phase += freq * freq / 1000000;
+                            while (channel.phase > 0) {
+                                channel.phase--;
+                                const bit0 = channel.noiseSeed & 1;
+                                channel.noiseSeed >>= 1;
+                                const bit1 = channel.noiseSeed & 1;
+                                const feedback = (bit0 ^ bit1);
+                                channel.noiseSeed |= feedback << 14;
+                                channel.noiseLastRandom = (feedback << 1) - 1;
+                            }
+                            sample = volume * channel.noiseLastRandom;
+
+                        } else {
+                            channel.phase += freq / SAMPLE_RATE;
+                            if (channel.phase > 1) {
+                                channel.phase--;
+                            }
+
+                            if (channelIdx == 2) {
+                                // Triangle channel
+                                // if (channel.phase < 0.5) {
+                                //     sample = lerp(-volume, volume, 2*channel.phase);
+                                // } else {
+                                //     sample = lerp(volume, -volume, 2*channel.phase - 1);
+                                // }
+                                // TODO(2022-01-16): Fix popping
+                                if (channel.phase < 0.25) {
+                                    sample = lerp(0, volume, 4*channel.phase);
+                                } else if (channel.phase < 0.75) {
+                                    sample = lerp(volume, -volume, 2*channel.phase - 0.5);
+                                } else {
+                                    sample = lerp(-volume, 0, 4*channel.phase - 3);
+                                }
+
+                            } else {
+                                // Pulse channel
+                                sample = channel.phase < channel.pulseDutyCycle ? volume : -volume;
+                            }
+                        }
+
+                        sum += sample;
+                    }
+                }
+
+                output[ii] = sum;
             }
-            return ctx.createPeriodicWave(real, new Float32Array(DUTY_CYCLE_LENGTH));
-        }
-        const duty25 = createDutyCycle(0.25);
-        this.pulseDutyCycles = [
-            createDutyCycle(0.125),
-            duty25,
-            createDutyCycle(0.5),
-            duty25, // 75 is the same as 25
-        ];
+        });
+        scriptNode.connect(ctx.destination);
+    }
 
-        const noiseBuffer = ctx.createBuffer(1, NOISE_LENGTH, ctx.sampleRate);
-        const noiseData = noiseBuffer.getChannelData(0);
-        let seed = 0x0001;
-        for (let ii = 0; ii < NOISE_LENGTH; ++ii) {
-            const bit0 = seed & 1;
-            seed >>= 1;
-            const bit1 = seed & 1;
-            const feedback = (bit0 ^ bit1);
-            seed |= feedback << 14;
-            noiseData[ii] = 2*feedback - 1;
+    ramp (value1, value2, time1, time2) {
+        const t = (this.time - time1) / (time2 - time1);
+        return lerp(value1, value2, t);
+    }
+
+    getCurrentFrequency (channel) {
+        if (channel.freq2 > 0) {
+            return this.ramp(channel.freq1, channel.freq2, channel.startTime, channel.releaseTime);
+        } else {
+            return channel.freq1;
         }
-        this.noiseBuffer = noiseBuffer;
+    }
+
+    getCurrentVolume (channel) {
+        const time = this.time;
+        if (time > channel.sustainTime) {
+            return this.ramp(channel.volume, 0, channel.sustainTime, channel.releaseTime);
+        } else if (time > channel.decayTime) {
+            return channel.volume;
+        } else if (time > channel.attackTime) {
+            return this.ramp(MAX_VOLUME, channel.volume, channel.attackTime, channel.decayTime);
+        } else {
+            return this.ramp(0, MAX_VOLUME, channel.startTime, channel.attackTime);
+        }
     }
 
     tone (frequency, duration, volume, flags) {
         const freq1 = frequency & 0xffff;
         const freq2 = (frequency >> 16) & 0xffff;
 
-        const sustain = (duration & 0xff) / 60;
-        const release = ((duration >> 8) & 0xff) / 60;
-        const decay = ((duration >> 16) & 0xff) / 60;
-        const attack = ((duration >> 24) & 0xff) / 60;
+        const sustain = (duration & 0xff);
+        const release = ((duration >> 8) & 0xff);
+        const decay = ((duration >> 16) & 0xff);
+        const attack = ((duration >> 24) & 0xff);
 
-        const channel = flags & 0x3;
+        const channelIdx = flags & 0x3;
         const mode = (flags >> 2) & 0x3;
 
-        const ctx = this.ctx;
-        const attackTime = ctx.currentTime + attack;
-        const decayTime = attackTime + decay;
-        const sustainTime = decayTime + sustain;
-        const releaseTime = sustainTime + release;
+        const channel = this.channels[channelIdx];
+        channel.freq1 = freq1;
+        channel.freq2 = freq2;
+        channel.startTime = this.time;
+        channel.attackTime = channel.startTime + ((SAMPLE_RATE*attack/60) >>> 0);
+        channel.decayTime = channel.attackTime + ((SAMPLE_RATE*decay/60) >>> 0);
+        channel.sustainTime = channel.decayTime + ((SAMPLE_RATE*sustain/60) >>> 0);
+        channel.releaseTime = channel.sustainTime + ((SAMPLE_RATE*release/60) >>> 0);
+        channel.volume = MAX_VOLUME * volume/100;
 
-        // Peak level of any channel should be -12 dB, or a quarter of full volume
-        let peakLevel = 0.25;
-        let sustainLevel = volume/100 * peakLevel;
-
-        let node;
-        const existingNode = this.nodes[channel];
-        if (existingNode != null) {
-            node = existingNode;
-        } else {
-            // create a new node
-            if (channel == 3) {
-                node = ctx.createBufferSource();
-                node.buffer = this.noiseBuffer;
-                node.loop = true;
-            } else {
-                node = ctx.createOscillator();
-                if (channel == 2) {
-                    node.type = "triangle";
-                }
-            }
-            this.nodes[channel] = node;
-            node.addEventListener('ended', () => {this.nodes[channel] = null;}); //note: might want accesses to be atomic
-            node.start(0);
-        }
-
-        // update a node, whether new or reused
-        if (channel == 3) {
-            const pbrValue = node.playbackRate;
-            pbrValue.cancelScheduledValues(0);
-            pbrValue.setValueAtTime(freq1*freq1/1000000, 0);
-            // pbrValue.value = NOISE_TABLE[16*(freq1/1024) | 0];
-            if (freq2) {
-                pbrValue.linearRampToValueAtTime(freq2*freq2/1000000, releaseTime);
-            }
-
-        } else {
-            if (channel != 2) {
-                const wave = this.pulseDutyCycles[mode];
-                node.setPeriodicWave(wave);
-            }
-
-            const freqValue = node.frequency;
-            freqValue.cancelScheduledValues(0);
-            freqValue.setValueAtTime(freq1, 0);
-            if (freq2) {
-                freqValue.linearRampToValueAtTime(freq2, releaseTime);
+        if (channelIdx == 0 || channelIdx == 1) {
+            switch (mode) {
+            case 0:
+                channel.pulseDutyCycle = 0.125;
+                break;
+            case 1: case 3: default:
+                channel.pulseDutyCycle = 0.25;
+                break;
+            case 2:
+                channel.pulseDutyCycle = 0.5;
+                break;
             }
         }
-        node.stop(releaseTime); // replaces any previous stop delay
-
-        // Create gain node with ADSR ramp
-        let gain;
-        const existingGain = this.gains[channel];
-        if (existingGain != null) {
-            gain = existingGain;
-            existingGain.gain.cancelScheduledValues(0);
-        } else {
-            gain = ctx.createGain();
-            this.gains[channel] = gain;
-            gain.connect(ctx.destination);
-        }
-        node.connect(gain);
-
-        const gainValue = gain.gain;
-        if(existingNode == null){
-            gainValue.setValueAtTime(0, 0);
-        }
-        gainValue.linearRampToValueAtTime(peakLevel, attackTime);
-        gainValue.linearRampToValueAtTime(sustainLevel, decayTime);
-        gainValue.linearRampToValueAtTime(sustainLevel, sustainTime);
-        gainValue.linearRampToValueAtTime(0, releaseTime);
     }
 }

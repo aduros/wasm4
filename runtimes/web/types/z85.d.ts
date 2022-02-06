15798c7373a3f54d666d5b04b228c6fec829c6d6
FaberVitale <faber.vitale@gmail.com>
2022-02-06 18:17:32 +0100

    fix(web-runtime): remove  types/ folder

--- runtimes/web/types/z85.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function encode(src: number[] | Uint8Array | Uint8ClampedArray): string;
-export declare function decode(string: string, dest: number[] | Uint8Array | Uint8ClampedArray): number;

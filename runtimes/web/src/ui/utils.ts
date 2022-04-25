a92b8ee4d2c2bb4344619a8360138a3bbc0caf20
Bruno Garcia <b@aduros.com>
2022-04-25 01:04:56 -0300

    Expand the iframe on iOS as a pseudo-fullscreen.

--- runtimes/web/src/ui/utils.ts
+++ runtimes/web/src/ui/utils.ts
@@ -14,7 +14,26 @@ export function getUrlParam (name: string): string | null {
 
 export function requestFullscreen () {
     if (document.fullscreenElement == null) {
-        document.body.requestFullscreen({navigationUI: "hide"});
+        function expandIframe () {
+            // Fullscreen failed, try to maximize our own iframe. We don't yet have a button to go
+            // back to minimized, but this at least makes games on wasm4.org playable on iPhone
+            const iframe = window.frameElement;
+            if (iframe) {
+                iframe.style.position = "fixed";
+                iframe.style.top = "0";
+                iframe.style.left = "0";
+                iframe.style.zIndex = "99999";
+                iframe.style.width = "100%";
+                iframe.style.height = "100%";
+            }
+        }
+
+        const promise = document.body.requestFullscreen && document.body.requestFullscreen({navigationUI: "hide"});
+        if (promise) {
+            promise.catch(expandIframe);
+        } else {
+            expandIframe();
+        }
     }
 }
 

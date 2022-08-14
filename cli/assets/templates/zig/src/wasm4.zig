41e2ad74da1ec8d115afc9236c1c0bbead9de3aa
Marcus Ramse <marcus@ramse.se>
2022-08-14 16:36:27 +0200

    Zig template: Use u32 for all widths and heights

--- cli/assets/templates/zig/src/wasm4.zig
+++ cli/assets/templates/zig/src/wasm4.zig
@@ -49,10 +49,10 @@ pub const SYSTEM_HIDE_GAMEPAD_OVERLAY: u8 = 2;
 // └───────────────────────────────────────────────────────────────────────────┘
 
 /// Copies pixels to the framebuffer.
-pub extern fn blit(sprite: [*]const u8, x: i32, y: i32, width: i32, height: i32, flags: u32) void;
+pub extern fn blit(sprite: [*]const u8, x: i32, y: i32, width: u32, height: u32, flags: u32) void;
 
 /// Copies a subregion within a larger sprite atlas to the framebuffer.
-pub extern fn blitSub(sprite: [*]const u8, x: i32, y: i32, width: i32, height: i32, src_x: u32, src_y: u32, stride: i32, flags: u32) void;
+pub extern fn blitSub(sprite: [*]const u8, x: i32, y: i32, width: u32, height: u32, src_x: u32, src_y: u32, stride: u32, flags: u32) void;
 
 pub const BLIT_2BPP: u32 = 1;
 pub const BLIT_1BPP: u32 = 0;
@@ -64,7 +64,7 @@ pub const BLIT_ROTATE: u32 = 8;
 pub extern fn line(x1: i32, y1: i32, x2: i32, y2: i32) void;
 
 /// Draws an oval (or circle).
-pub extern fn oval(x: i32, y: i32, width: i32, height: i32) void;
+pub extern fn oval(x: i32, y: i32, width: u32, height: u32) void;
 
 /// Draws a rectangle.
 pub extern fn rect(x: i32, y: i32, width: u32, height: u32) void;

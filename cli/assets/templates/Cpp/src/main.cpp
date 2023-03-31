0d84ac08000ccbd3348563098c6051490f4a0cc5
AbdulKalam21 <abdulkalam2185@gmail.com>
2023-03-31 09:54:19 +0530

    Fix a bug in C template and added C++ template

---
+++ cli/assets/templates/Cpp/src/main.cpp
@@ -0,0 +1,25 @@
+#include "wasm4.h"
+
+const uint8_t smiley[] = {
+    0b11000011,
+    0b10000001,
+    0b00100100,
+    0b00100100,
+    0b00000000,
+    0b00100100,
+    0b10011001,
+    0b11000011,
+};
+
+void update () {
+    *DRAW_COLORS = 2;
+    text("Hello from C++!", 10, 10);
+
+    uint8_t gamepad = *GAMEPAD1;
+    if (gamepad & BUTTON_1) {
+        *DRAW_COLORS = 4;
+    }
+
+    blit(smiley, 76, 76, 8, 8, BLIT_1BPP);
+    text("Press X to blink", 16, 90);
+}

7cea5274b8c0eb71d3e0ef406938c488e3fbfb20
Benjamin Collet <benjamin.collet@protonmail.ch>
2024-01-17 14:45:48 +0100

    zig: Fix template build

--- cli/assets/templates/zig/build.zig
+++ cli/assets/templates/zig/build.zig
@@ -4,15 +4,15 @@ pub fn build(b: *std.Build) !void {
     const exe = b.addExecutable(.{
         .name = "cart",
         .root_source_file = .{ .path = "src/main.zig" },
-        .target = .{
+        .target = b.resolveTargetQuery(.{
             .cpu_arch = .wasm32,
             .os_tag = .freestanding,
-        },
+        }),
         .optimize = b.standardOptimizeOption(.{}),
     });
 
     exe.entry = .disabled;
-    exe.export_symbol_names = &[_][]const u8{ "start", "update" };
+    exe.root_module.export_symbol_names = &[_][]const u8{ "start", "update" };
     exe.import_memory = true;
     exe.initial_memory = 65536;
     exe.max_memory = 65536;

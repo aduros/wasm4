2a348f21044cb32aa99b75763234dba5abf243b6
Matt Harding <majaharding@gmail.com>
2024-11-19 21:38:46 +0000

    Don't try to open a websocket when doing platform dev
    When we're developing the wasm4 web runtime itself, there's no w4 CLI
    websocket to connect to. But trying to connect anyway fights for the
    Vite websocket, which causes hangs on reload occasionally.

--- runtimes/web/src/devkit.ts
+++ runtimes/web/src/devkit.ts
@@ -1,4 +1,9 @@
 import * as constants from "./constants";
-export const websocket = constants.GAMEDEV_MODE
+// True if we're developers of the WASM4 runtime itself, and are running the runtime
+// using `npm start` or `vite` etc. In this case, there's no wasm4 CLI web socket to
+// connect to, but we might be fighting for the Vite websocket.
+const PLATFORM_DEVELOPER_MODE = import.meta.env.MODE === "development";
+
+export const websocket = constants.GAMEDEV_MODE && !PLATFORM_DEVELOPER_MODE
     ? new WebSocket((location.protocol == "https:" ? "wss" : "ws") + "://" + location.host)
     : null;
